c2caf4d4191db87a8c750d1f1cbeabab18bc7af1
Brian Canada, PhD <brian.canada@gmail.com>
2025-01-15 06:49:08 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Welcome to My React App!</h1>
+      <p>This is a simple React app hosted on Netlify or Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
